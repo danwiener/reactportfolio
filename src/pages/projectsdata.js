b3eaf9318d7e718c55032f0c8db1f113bc5e3ddd
Henrik Wiener <henrik.wiener@outlook.com>
2022-12-31 00:22:55 -0800

    added new url

--- src/pages/projectsdata.js
+++ src/pages/projectsdata.js
@@ -28,7 +28,7 @@ const projectsdata = [
 	image: "./resume.svg",
 	description:
 		"React.js/CSS/Bootstrap/HTML5 portfolio website built to demonstrate my projects , passions , and skills , utilizing React Components , React Routing , React States , Neumorphic Designs , Adobe Suite , React-Icons , extensive CSS/Bootstrap conditional styling and animations , and my body of work",
-	link: "https://www.henrikwiener-portfolio.com/",
+	link: "https://www.henrikwiener.com/",
     futurechanges: "Future features: enhance accessbility for hearing/visually impaired , improve UI responsiveness to mobile device viewing , advanced animations and extensive Three.js/Web GL graphics throughout for a fully immersive , interactive user-experience",
 	},
     {
